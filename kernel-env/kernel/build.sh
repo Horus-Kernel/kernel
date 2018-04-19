d51f27db8c8a17a2719988957dbf2a62ef10e177
warrior-graph <hmarques979@gmail.com>
2018-04-19 17:37:55 -0300

    TODO: fix this https://pastebin.com/t41r2P7H

--- kernel-env/kernel/build.sh
+++ kernel-env/kernel/build.sh
@@ -5,14 +5,14 @@ export CROSS_COMPILE=../$1
 export ARCH=arm64
 if [[ $2 = "full" ]]; then
 	make mrproper
-	make defconfig fanscoo.config
+	make defconfig fet.config
 fi
-make KERNELRELEASE=kernel-16.09-qcom Image dtbs  -j$[$(nproc) + 1]
+make KERNELRELEASE=4.4.23-linaro-lt-qcom Image dtbs  -j$[$(nproc) + 1]
 #rm ./arch/arm64/boot/dts/qcom/apq8016-var-dart.dtb
 #rm ./arch/arm64/boot/dts/qcom/apq8016-sbc.dtb
 if [[ $2 = "full" ]]; then
-	make KERNELRELEASE=kernel-16.09-qcom modules  -j$[$(nproc) + 1] 
-	make KERNELRELEASE=kernel-16.09-qcom INSTALL_MOD_PATH=build-${ARCH}-modules modules_install -j$[$(nproc) + 1] 
+	make KERNELRELEASE=4.4.23-linaro-lt-qcom modules  -j$[$(nproc) + 1] 
+	make KERNELRELEASE=4.4.23-linaro-lt-qcom INSTALL_MOD_PATH=build-${ARCH}-modules modules_install -j$[$(nproc) + 1] 
 	sudo find ./build-arm64-modules/ -type l -delete
 	cd ./build-arm64-modules/
 	pwd
--- kernel-env/makebootimg.sh
+++ kernel-env/makebootimg.sh
@@ -18,30 +18,25 @@ esac
 
 export CROSS_COMPILE=gcc-linaro-6.3.1-2017.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 export ARCH=arm64
-export PATH=~/bin:$PATH
-export PATH=~/bin/skales:$PATH
 cd ./kernel
 ./build.sh $CROSS_COMPILE $1
 cd ..
 ./skales/dtbTool -o dt.img -s 2048 kernel/arch/arm64/boot/dts/qcom/
-export cmdline="root=/dev/mmcblk0p10 rw rootwait console=tty0 console=ttyMSM0,115200n8 drm.debug=15 loglevel=10"
 ./skales/mkbootimg --kernel kernel/arch/arm64/boot/Image \
-          --ramdisk ramdisk/initrd.img-4.9.56-linaro-lt-qcom \
-          --output boot-sd410.img \
-          --dt dt.img \
-          --pagesize 2048 \
-          --base 0x80000000 \
-          --cmdline "$cmdline"
+                   --ramdisk ramdisk/initrd.img-4.4.23-linaro-lt-qcom \
+                   --output boot-sd410.img \
+                   --dt dt.img \
+                   --pagesize 2048 \
+                   --base 0x80000000 \
+                   --cmdline "root=/dev/mmcblk0p10 rw rootwait console=ttyMSM0,115200n8"
 echo "Image: boot-sd410.img. Done!"
 
-echo "Do you want to copy 'boot-sd410.img' to sdcard? [y/n]"
+echo "Do you want to flash 'boot-sd410.img'? [y/n]"
 
 read -n1 ans
 if [[ $ans = "y" ]]; then
     echo "Copying image to sdcard"
-    sudo cp boot-sd410.img /media/$USER/OSes/Debian/
-    sudo umount /media/$USER/OSes && sudo umount /media/$USER/rootfs
-    rm dt.img
+    sudo fastboot flash boot boot-sd410.img
     echo Success
  
 fi
